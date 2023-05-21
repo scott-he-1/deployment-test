e0f19e6afa2f38f52ce17140101a995a75e5412b
Scott He <scotthe234@gmail.com>
2023-05-21 03:12:56 -0400

    deploy

--- main.js
+++ main.js
@@ -4,8 +4,21 @@ const app = require("express")();
 const port = 3000;
 
 app.get("/", async (req, res) => {
-  const users = await prisma.user.findMany();
   return res.send("Hello World!");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  return res.send(users);
+});
+
+app.post("/users", async (req, res) => {
+  const newUser = await prisma.user.create({
+    data: {
+      email: "test@test.com",
+      passwordHash: "passwordHash",
+    },
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
